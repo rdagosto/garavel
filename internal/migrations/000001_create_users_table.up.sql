e54044ddcddb9d6c3c5e9d123121de74513915bc
Rodrigo <rdagosto@gmail.com>
2025-03-15 15:46:22 +0200

    cleanning migrations

--- internal/migrations/000001_create_users_table.up.sql
+++ internal/migrations/000001_create_users_table.up.sql
@@ -5,6 +5,3 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
-
-INSERT INTO `users` (`id`, `email`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'test@test.com', '$2a$10$vXBKrWibH.ReU7xiNrJkVucKEZrK3GYZKk9e0mmd870pnOFP8DxHG', '2025-03-09 18:45:07', '2025-03-09 18:45:07');
